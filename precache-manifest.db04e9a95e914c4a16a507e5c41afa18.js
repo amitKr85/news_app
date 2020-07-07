f9e88b5885a29c7d971facdc3650e5977d171e02
B0218957 <b0218957@LTB0218957-Mac.local>
2020-07-07 15:17:49 +0530

    Updates

---
+++ precache-manifest.db04e9a95e914c4a16a507e5c41afa18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac7f29fea34472785ebbee6742ac5ff",
+    "url": "/news_app/index.html"
+  },
+  {
+    "revision": "0368cb076bec6b2b5532",
+    "url": "/news_app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "717a1fa014b529236e18",
+    "url": "/news_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0368cb076bec6b2b5532",
+    "url": "/news_app/static/js/2.a40ca9f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news_app/static/js/2.a40ca9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717a1fa014b529236e18",
+    "url": "/news_app/static/js/main.2143ee5b.chunk.js"
+  },
+  {
+    "revision": "ee3cb7f0b86e67490bef",
+    "url": "/news_app/static/js/runtime-main.a4a62132.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
